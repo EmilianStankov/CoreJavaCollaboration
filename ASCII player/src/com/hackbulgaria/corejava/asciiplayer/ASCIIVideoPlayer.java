631aedbc578f21492914bca5c9389339a4437aa9
EmilianStankov <eminstine@abv.bg>
2014-07-03 14:13:47 +0300

    Fixing the factory

---
+++ ASCII player/src/com/hackbulgaria/corejava/asciiplayer/ASCIIVideoPlayer.java
@@ -0,0 +1,17 @@
+package com.hackbulgaria.corejava.asciiplayer;
+
+import java.io.File;
+
+public class ASCIIVideoPlayer implements ASCIIPlayer {
+
+    public ASCIIVideoPlayer(File file) {
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public String play() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
--- ASCII player/src/com/hackbulgaria/corejava/asciiplayer/PlayerFactory.java
+++ ASCII player/src/com/hackbulgaria/corejava/asciiplayer/PlayerFactory.java
@@ -1,22 +1,29 @@
 package com.hackbulgaria.corejava.asciiplayer;
-import java.io.File;
 
+import java.io.File;
+import java.io.IOException;
 
 public class PlayerFactory {
-    
-    public static ASCIIPlayer newAsciiPlayer (File file) {
-        
+
+    public static ASCIIPlayer newAsciiPlayer(File file) throws IOException {
+
         String fileName = file.getName();
-        String extention = fileName.substring(fileName.lastIndexOf(".")+1);
+        String extention = fileName.substring(fileName.lastIndexOf(".") + 1);
         ASCIIPlayer result;
-        
-        switch(extention) {
-            case "jpg" :    result = new ASCIIPicturePlayer(file);
-                            break;
-            case "gif" :    result = new ASCIIGifPlayer(file);
-                            break;
-            case "mp4" :    result = new ASCIIVideoPlayer(file);
-                            break;
+
+        switch (extention) {
+            case "jpg":
+                result = new ASCIIPicturePlayer(file);
+                break;
+            case "gif":
+                result = new ASCIIGifPlayer(file);
+                break;
+            case "mp4":
+                result = new ASCIIVideoPlayer(file);
+                break;
+            default:
+                result = new ASCIIPicturePlayer(file);
         }
+        return result;
     }
 }
